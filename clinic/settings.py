3a86093f1b5144f20ee789b8a029090cc6245436
tmsundarrajan <sundar192114l@gmail.com>
2025-09-24 21:29:59 +0530

    update settings.py

--- clinic/settings.py
+++ clinic/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = 'dashboard'
+
+LOGUT_REDIECT_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
