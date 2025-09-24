ad0d4b8d8bdd415c9103a5809f6976ada7a26e51
tmsundarrajan <sundar192114l@gmail.com>
2025-09-24 22:42:37 +0530

    update settings.py

--- clinic/settings.py
+++ clinic/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +125,8 @@ STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'dashboard'
 
 LOGUT_REDIECT_URL = 'login'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
