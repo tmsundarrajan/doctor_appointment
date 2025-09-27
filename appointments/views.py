b752c1b58073dc63ebe3d90682d091067d1c32e1
tmsundarrajan <sundar192114l@gmail.com>
2025-09-27 15:42:38 +0530

    create book_appointment api method

--- appointments/views.py
+++ appointments/views.py
@@ -16,5 +16,21 @@ def dashboard(request):
         appointments=None
     return render(request, 'dashboard.html', {'appointments':appointments})
 
+@login_required
+def book_appointment(request):
+    if request.method=="POST":
+        doctor_id=request.POST['doctor']
+        date=request.POST['date']
+        time=request.POST['time']
+        symptoms=request.POST['symptoms']
+        
+        doctor=Doctor.objects.get(id=doctor_id)
+        patient=request.user.patient
+        
+        Appointment.objects.create(doctor=doctor, patient=patient, date=date, time=time, symptoms=symptoms)
+        return redirect('dashboard')
+    
+    doctors=Doctor.objects.all()
+    return render(request, 'book_appointment.html', {'doctors':doctors})
 
 
