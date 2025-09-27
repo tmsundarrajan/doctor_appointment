a341d8682d3eef99711e7f204e0dec5da9809300
tmsundarrajan <sundar192114l@gmail.com>
2025-09-27 15:27:47 +0530

    create dashboard api method

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Doctor, Patient, Appointment
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def dashboard(request):
+    if hasattr(request.user, 'doctor'):
+        appointments=Appointment.objects.filter(doctor=request.user.doctor)
+    elif hasattr(request.user, 'patient'):
+        appointments=Appointment.objects.filter(patient=request.user.patient)
+    else:
+        appointments=None
+    return render(request, 'dashboard.html', {'appointments':appointments})
+
+
+
