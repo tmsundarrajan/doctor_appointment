dd420485f7344854c32022110b432049de4642b7
tmsundarrajan <sundar192114l@gmail.com>
2025-10-01 22:52:47 +0530

    create app urls file and create urls

---
+++ appointments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('book/', views.book_appointment, name='book_appointment',),
+]
